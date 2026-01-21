5ecdbc210b9c324c5ca56180a800c66a8865ea10
Phmore <patryckhenryck401@gmail.com>
2026-01-21 20:12:07 -0300

    primeiro commit

--- GoogleLogin/core/adapters.py
+++ GoogleLogin/core/adapters.py
@@ -6,3 +6,13 @@ class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
         sociallogin.state['next'] = '/api/token/'
         return user
 
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from django.conf import settings
+
+class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
+    
+    # Este método é quem decide para onde ir após o login com sucesso
+    def get_login_redirect_url(self, request):
+        # Aqui você retorna a URL que deseja.
+        # Pode ser hardcoded '/api/token/' ou usar reverse()
+        return '/api/token/'
